82bfe7c8f5ea0881cb6e292237e3d8f2950fa2dc
Juan Camilo Rodriguez Montoya <jrodri92@hdpmaster.dis.eafit.edu.co>
2018-11-21 17:02:07 -0500

    mostrando datos desordenados y pidiendolos por consola

--- serial/indinv-serial.py
+++ serial/indinv-serial.py
@@ -1,10 +1,16 @@
 import pandas as pd
 
 def mining():
-    data = pd.read_csv("/opt/datasets/articles1.csv")
-    print(data)
-
-
+    try:
+        sub = input("Entrar la palabra entre \" para quitar \"/\" : " )
+        if(sub == "/"):
+            return 0
+        data = pd.read_csv("/opt/datasets/articles1.csv",usecols=[1,2,9])
+        data["frec"] = data["content"].str.find(sub)
+        print(data)
+    except:
+        mining()
+        
 
 
 #def insertList():
