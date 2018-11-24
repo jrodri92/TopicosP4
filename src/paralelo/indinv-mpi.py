f1fc6ccb8d7d34dccc1bf496dfae77748a24aa91
jrodri92 <jrodri92@eafit.edu.co>
2018-11-23 22:52:11 -0500

    documentando parte del codigo

--- src/paralelo/indinv-mpi.py
+++ src/paralelo/indinv-mpi.py
@@ -6,26 +6,31 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 direc = "/opt/datasets/articles" + str(rank+1) + ".csv"
 
-#inicializando el archivo correspondiente
-if rank <= 2:
-    data = pd.read_csv(direc,usecols=[1,2,9])
-    data["frec"] = data["content"].str.count(sub.upper()) 
-    + data["content"].str.count(sub.lower()) 
-    + data["content"].str.count(sub.capitalize())
 
-#pedir la palabra
+#pedir la palabra y enviarla
 if rank == 0:
     sub = input("Ingrese la palabra, para salir ingrese \"/\" : " )
     comm.send(sub,dest=1)
     comm.send(sub,dest=2)
-    
+
+#recibir la palabra    
 elif rank == 1:
     sub = comm.recv(source = 0)
 
 elif rank == 2:
     sub = comm.recv(source = 0) 
 
+
 if rank <= 2:
+    #inicializando el archivo correspondiente
+    data = pd.read_csv(direc,usecols=[1,2,9])
+
+    #calculando la frecuencia de la palabra
+    data["frec"] = data["content"].str.count(sub.upper()) 
+    + data["content"].str.count(sub.lower()) 
+    + data["content"].str.count(sub.capitalize())
+    
+    #organizando de menor a mayor el archivo
     data = data.sort_values(by='frec',ascending=False)
     if rank != 0:
         comm.send(data.iloc[0:10], dest = 0)
