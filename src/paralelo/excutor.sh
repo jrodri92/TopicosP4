fb4d841609b1389b89c33003f2e49844b6f3c795
jrodri92 <jrodri92@eafit.edu.co>
2018-11-23 22:41:27 -0500

    prueba de la paralelizacion

--- src/paralelo/excutor.sh
+++
@@ -1,7 +0,0 @@
-if ! [ -x /usr/bin/nproc ]; then
-    echo "nproc is not installed. Please install it."
-    exit 1
-fi
-PYTHON_HOME=/opt/anaconda3/bin
-CORES=$(nproc)
-EXAMPLE=09 mpiexec -f ../../hosts_mpi -np ${CORES} ${PYTHON_HOME}/python ./${EXAMPLE}*.py
--- src/paralelo/indinv-mpi.py
+++ src/paralelo/indinv-mpi.py
@@ -1,19 +1,41 @@
 from mpi4py import MPI
 import pandas as pd
 
+#inicializacion de variables
 comm = MPI.COMM_WORLD
 rank = comm-Get_rank()
+direc = "/opt/datasets/articles" + str(rank) + ".csv"
 
+#inicializando el archivo correspondiente
+if rank <= 2:
+    data = pd.read_csv(direc,usecols=[1,2,9])
+    data["frec"] = dataFinal["content"].str.count(sub.upper()) 
+            + dataFinal["content"].str.count(sub.lower()) 
+            + dataFinal["content"].str.count(sub.capitalize())
+
+#pedir la palabra
 if rank == 0:
     sub = input("Ingrese la palabra, para salir ingrese \"/\" : " )
     comm.send(sub,dest=1)
     comm.send(sub,dest=2)
-
-    data = pd.read_csv("/opt/datasets/articles1.csv",usecols=[1,2,9])
-else if rank == 1:
+    
+elif rank == 1:
     sub = comm.recv(source = 0)
-    data = pd.read_csv("/opt/datasets/articles2.csv",usecols=[1,2,9])
 
-else if rank == 2:
-    sub = comm.recv(source = 0)
-    data = pd.read_csv("/opt/datasets/articles3.csv",usecols=[1,2,9]) 
+elif rank == 2:
+    sub = comm.recv(source = 0) 
+
+if rank <= 2:
+    data = data.sort_values(by='frec',ascending=False)
+    if rank != 0:
+        comm.send(data.iloc[0:10], dest = 0)
+    else:
+        data = data.iloc[0:10]
+        data1 = comm.recv(source = 1)
+        data2 = comm.recv(source = 2)
+        print "------------0------------"
+        print data
+        print "-------------1--------------"
+        print data1
+        print "---------------------2---------------"
+        print data2
